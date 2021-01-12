62c3eee51a02f96c9db8684b624f3faf59badd4c
Johan Esteban <johesteb@gmail.com>
2021-01-12 00:50:18 -0300

    update

--- main.py
+++ main.py
@@ -13,17 +13,40 @@ def read_file(year, drops):
     print('Reading dataset', year)
     # Leemos los datos y separamos por ; porque algunos nombres de establecimientos poseen comas y dan error
     df = pd.read_csv("datasets/Rendimiento por estudiante "+str(year)+".csv", sep=';', low_memory=False, encoding='latin-1')
-    #df_len = df['NOM_RBD'].count()
+    print('Cantidad de datos:',len(df))
 
-    # Eliminamos columna de Código de sector económico (sólo media técnico-profesional y artística)
-    # Eliminamos columna de Código de especialidad (sólo media técnico-profesional y artística)
-    # No nos sirve para el análisis de datos final y contienen datos vacíos en algunos casos
-    #df = df.drop(columns=drops)
-    #ram_info = 'Dataframe ' + str(year)
-    df_2010_head = df_2010.head()
-    html_2010 = df_2010_head.to_html("datasets_headers_pdf/df_"+year+".html")
+    
+    # Eliminamos las columnas
+    if drops!=None:
+        df = df.drop(columns=drops)
+
+  
+    df["SIT_FIN"].fillna( method ='-', inplace = True) # Esta en todos los años
+    """
+    df["COD_SEC"].fillna( method ='0', inplace = True) # Esta en todos los años
+    df["COD_ESPE"].fillna( method ='0', inplace = True) # Esta en todos los años
+    if year <= 2013:
+        df['INT_ALU'] = df['INT_ALU'].replace(['.'],'2') # Esta solo en los años 2010-2013
+    
+    #df_com_rbd = df.copy()
+    """
+
+    """
+    data_com_rbd = [df["COD_COM_ALU"], df["NOM_COM_RBD"]]
+    headers_com_rbd = ["COD_COM_ALU", "NOM_COM_RBD"]
+
+    df3 = pd.concat(data_com_rbd, axis=1, keys=headers_com_rbd)
+    print(df3)
+    """
+    
+
+    #df_2010_head = df_2010.head()
+    #html_2010 = df_2010_head.to_html("datasets_headers_pdf/df_"+year+".html")
     ram(info='Dataframe ' + str(year))
     return df
+
+
+
 
 def insert_dimensions():
     q.drop_static_tables() # Elimina las tablas estáticas si existen
@@ -57,8 +80,8 @@ if __name__ == "__main__":
     ram(info='Delete imports')
 
     # Leemos los archivos
-    
-    df_2010 = read_file(year=2010, drops=['COD_SEC', 'COD_ESPE'])
+    df_2010 = read_file(year=2010, drops=['SIT_FIN_R'])
+    """
     df_2011 = read_file(year=2011, drops=['COD_SEC', 'COD_ESPE', 'FEC_ING_ALU'])
     df_2012 = read_file(year=2012, drops=['COD_SEC', 'COD_ESPE'])
     df_2013 = read_file(year=2013, drops=['COD_SEC', 'COD_ESPE'])
@@ -68,7 +91,7 @@ if __name__ == "__main__":
     df_2017 = read_file(year=2017, drops=['COD_SEC', 'COD_ESPE'])
     df_2018 = read_file(year=2018, drops=['COD_SEC', 'COD_ESPE'])
     df_2019 = read_file(year=2019, drops=['COD_SEC', 'COD_ESPE'])
-
+    """
 
     ram(info='Final ram usage 2010-2019')
 
